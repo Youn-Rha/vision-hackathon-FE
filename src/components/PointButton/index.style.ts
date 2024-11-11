38aed33baa0e6b46143239958986fdfe31c079b7
chominju <cmj021002@knu.ac.kr>
2024-11-12 03:11:31 +0900

    feat: pointButton 컴포넌트 구현

---
+++ src/components/PointButton/index.style.ts
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.button`
+    width: 110px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    background-color: transparent;
+
+    gap: 5px;
+`;
+
+export const Button = styled.button`
+    width: 100%;
+    height: 97px;
+
+    background-color: var(--color-white);
+
+    font-size: var(--font-size-s);
+    font-weight: bold;
+
+    border: none;
+    border-radius: 15px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+
+    padding: 10px;
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+export const Icon = styled.img`
+    width: fit-content;
+    height: fit-content;
+`;
