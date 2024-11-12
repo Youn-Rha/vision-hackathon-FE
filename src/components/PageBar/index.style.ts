cca2736ede961d333c2ca720aac1dbcb11df9b77
ry0218 <ry0218@naver.com>
2024-11-12 19:38:51 +0900

    feat: PageBar 컴포넌트 구현

---
+++ src/components/PageBar/index.style.ts
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+    height: 78px;
+    width: 440px;
+    background-color: var(--color-white);
+    border-bottom-style: solid;
+    border-bottom-width: 3px;
+    border-color: var(--color-primary);
+    box-shadow: 0px 4px 4px #00000040;
+    display: flex;
+    align-items: center;
+    gap: 12px
+`;
+
+export const Arrow = styled.img`
+    height: 31px;
+    width: 12px;
+    margin-left: 20px;
+`;
