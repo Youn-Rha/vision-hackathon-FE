3dbe6f4112ae09f551e267b5a9be538cdd4d9ffa
chominju <cmj021002@knu.ac.kr>
2024-11-12 18:07:26 +0900

    feat: Main style 추가

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,20 @@
 import { Outlet } from "react-router-dom";
 
+import styled from "@emotion/styled";
+
+export const Main = styled.main`
+    width: 100%;
+    max-width: 440px;
+    margin: 0 auto;
+    padding: 20px;
+`;
+
 export const RootLayout = () => {
     return (
         <>
-            <Outlet />
+            <Main>
+                <Outlet />
+            </Main>
         </>
     );
 };
