ce1290dea153ee48336fb6bb5c5d5b72df6ee4a7
chominju <cmj021002@knu.ac.kr>
2024-11-12 20:20:36 +0900

    feat: CharacterStartPage 라우팅 경로 추가

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -8,6 +8,7 @@ import { LoginPage } from "@/pages/LoginPage";
 import { resetStyles } from "@/styles/reset";
 import { rootStyles } from "@/styles/root";
 
+import { CharacterStartPage } from "./pages/CharacterStartPage";
 import { RootLayout } from "@/layouts/RootLayout";
 
 export const Router = () => {
@@ -18,6 +19,7 @@ export const Router = () => {
                 <Route path="/" element={<RootLayout />}>
                     <Route path="/login" element={<LoginPage />}></Route>
                     <Route path="/character/setting" element={<CharacterSettingPage />}></Route>
+                    <Route path="/character/start" element={<CharacterStartPage />}></Route>
                 </Route>
             </Routes>
         </>
