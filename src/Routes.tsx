24ae16deac9ef2d9812d70026529bcdce017c284
ry0218 <ry0218@naver.com>
2024-11-12 22:58:54 +0900

    feat: CheckStartPage 라우팅 경로 추가

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -9,6 +9,7 @@ import { resetStyles } from "@/styles/reset";
 import { rootStyles } from "@/styles/root";
 
 import { RootLayout } from "@/layouts/RootLayout";
+import { CheckStartPage } from "./pages/CheckStartPage";
 
 export const Router = () => {
     return (
@@ -18,6 +19,7 @@ export const Router = () => {
                 <Route path="/" element={<RootLayout />}>
                     <Route path="/login" element={<LoginPage />}></Route>
                     <Route path="/character/setting" element={<CharacterSettingPage />}></Route>
+                    <Route path="/checkstart" element={<CheckStartPage />}></Route>
                 </Route>
             </Routes>
         </>
