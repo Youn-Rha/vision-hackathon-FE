794d15b2b641bc29eebf6fdfd119772ad90c61db
Youn-Rha <ry0218@naver.com>
2024-11-13 03:32:22 +0900

    feat: MemoPage 라우팅 경로 추가

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -10,6 +10,7 @@ import { rootStyles } from "@/styles/root";
 
 import { RootLayout } from "@/layouts/RootLayout";
 import { MyPage } from "./pages/MyPage";
+import { MemoPage } from "./pages/MemoPage";
 
 export const Router = () => {
     return (
@@ -20,6 +21,7 @@ export const Router = () => {
                     <Route path="/login" element={<LoginPage />}></Route>
                     <Route path="/character/setting" element={<CharacterSettingPage />}></Route>
                     <Route path="/mypage" element={<MyPage />}></Route>
+                    <Route path="/memo" element={<MemoPage />}></Route>
                 </Route>
             </Routes>
         </>
