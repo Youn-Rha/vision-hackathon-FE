1ae536e27eef44c33e40826c8f652e15c001dfb9
chominju <cmj021002@knu.ac.kr>
2024-11-13 14:15:18 +0900

    style: PageBar height 수정

--- src/components/PageBar/index.style.ts
+++ src/components/PageBar/index.style.ts
@@ -1,19 +1,21 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
-    height: 58px;
-    width: 400px;
+    height: 70px;
+    width: 100%;
+
     background-color: var(--color-white);
     border-bottom-style: solid;
     border-bottom-width: 3px;
     border-color: var(--color-primary);
     box-shadow: 0px 4px 4px #00000040;
     display: flex;
+    align-items: center;
     gap: 12px;
 `;
 export const Arrow = styled.img`
-    height: 31px;
-    width: 12px;
+    height: 20px;
+    width: 10px;
     margin-left: 20px;
     display: inline-block;
 `;
