3143ca69a229aa3d97a60d0017af84263fa49e1c
ry0218 <ry0218@naver.com>
2024-11-13 15:17:45 +0900

    fix: CheckBar 구간 조정 및 마커 4개 표시

--- src/components/CheckBar/index.style.ts
+++ src/components/CheckBar/index.style.ts
@@ -2,8 +2,8 @@ import styled from "@emotion/styled";
 
 export const Container = styled.div`
     width: 300px;
-    height: 20px;
-    align-items: center;
+    position: relative;
+    height: 40px;
 `;
 
 export const TextContainer = styled.div`
@@ -13,13 +13,28 @@ export const TextContainer = styled.div`
 `;
 
 export const Slider = styled.input`
-    width: 300px;
+    width: 100%;
     height: 4px;
     border-radius: 2px;
-    top: 6px;
-    -webkit-appearance: none; /* 기본 스타일 제거 */
-    appearance: none; /* 모든 브라우저에서 기본 스타일 제거 */
-    background: var(--color-gray);
+    -webkit-appearance: none;
+    appearance: none;
+    background: linear-gradient(
+        to right,
+        var(--color-primary) 0%,
+        var(--color-primary) 2%,
+        var(--color-gray) 2%,
+        var(--color-gray) 32.3%,
+        var(--color-primary) 32.3%,
+        var(--color-primary) 34.3%,
+        var(--color-gray) 34.3%,
+        var(--color-gray) 64.6%,
+        var(--color-primary) 64.6%,
+        var(--color-primary) 66.6%,
+        var(--color-gray) 66.6%,
+        var(--color-gray) 98%,
+        var(--color-primary) 98%,
+        var(--color-primary) 100%
+    );
 
     &:active {
         cursor: grabbing;
@@ -29,31 +44,32 @@ export const Slider = styled.input`
         outline: none;
     }
 
-    /* WebKit 기반 브라우저에서 슬라이더 썸 스타일링 */
+    /* 슬라이더 썸 스타일 */
     ::-webkit-slider-thumb {
-        -webkit-appearance: none; /* 기본 스타일 제거 */
-        background: var(--color-primary); /* 원하는 색상으로 설정 */
-        width: 16px; /* 썸의 너비 */
-        height: 16px; /* 썸의 높이 */
-        border-radius: 50%; /* 동그라미 모양 */
-        border: 2px solid white; /* 선택 사항: 흰색 테두리 */
+        -webkit-appearance: none;
+        background: var(--color-primary);
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        border: 2px solid white;
+        cursor: pointer;
     }
 
-    /* Firefox에서 슬라이더 썸 스타일링 */
     &::-moz-range-thumb {
-        background: var(--color-primary); /* 원하는 색상으로 설정 */
-        width: 16px; /* 썸의 너비 */
-        height: 16px; /* 썸의 높이 */
-        border-radius: 50%; /* 동그라미 모양 */
-        border: 2px solid white; /* 선택 사항: 흰색 테두리 */
+        background: var(--color-primary);
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        border: 2px solid white;
+        cursor: pointer;
     }
 
-    /* Edge 및 기타 브라우저에서 슬라이더 썸 스타일링 */
     &::-ms-thumb {
-        background: var(--color-primary); /* 원하는 색상으로 설정 */
-        width: 16px; /* 썸의 너비 */
-        height: 16px; /* 썸의 높이 */
-        border-radius: 50%; /* 동그라미 모양 */
-        border: 2px solid white; /* 선택 사항: 흰색 테두리 */
+        background: var(--color-primary);
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        border: 2px solid white;
+        cursor: pointer;
     }
 `;
