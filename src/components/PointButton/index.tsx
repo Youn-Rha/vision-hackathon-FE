5568e4350628d2b309c74bf2476c18c0abe04c31
chominju <cmj021002@knu.ac.kr>
2024-11-13 12:52:11 +0900

    feat: PointButton variant를 입력하면 지정된 버튼으로 바로 생성되도록 설정 추가

--- src/components/PointButton/index.tsx
+++ src/components/PointButton/index.tsx
@@ -1,22 +1,41 @@
+import nutriendtsIcon from "@/assets/PointButton/nutrients.png";
+import sunIcon from "@/assets/PointButton/sun.png";
+import waterIcon from "@/assets/PointButton/water.png";
+
 import { Text } from "../Text";
 import * as Styles from "./index.style";
 
 export interface PointButtonProps {
-    variant: "water" | "sun" | "nutrients" | string;
-    children?: React.ReactNode;
-    iconSrc: string;
-    point: number;
+    variant: "water" | "sun" | "nutrients";
 }
 
 export const PointButton = (props: PointButtonProps) => {
+    const iconList = {
+        water: waterIcon,
+        sun: sunIcon,
+        nutrients: nutriendtsIcon,
+    };
+
+    const pointList = {
+        water: 10,
+        sun: 15,
+        nutrients: 20,
+    };
+
+    const actionList = {
+        water: "물주기",
+        sun: "햇빛 쐬기",
+        nutrients: "영양분",
+    };
+
     return (
         <Styles.Container>
             <Styles.Button>
-                {props.children}
-                <Styles.Icon src={props.iconSrc} alt={props.variant} /> {/* iconSrc를 src에 사용 */}
+                {actionList[props.variant]}
+                <Styles.Icon src={iconList[props.variant]} alt={props.variant} />
             </Styles.Button>
             <Text size="s" weight="normal" color="gray">
-                {props.point} point
+                {pointList[props.variant]} point
             </Text>
         </Styles.Container>
     );
