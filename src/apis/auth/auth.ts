8a164f511455891d19dc7dc487d346ae14731917
chominju <cmj021002@knu.ac.kr>
2024-11-14 05:24:15 +0900

    feat: useAithStore에 토큰 저장

--- src/apis/auth/auth.ts
+++ src/apis/auth/auth.ts
@@ -1,13 +1,12 @@
 import axiosInstance from "../axiosInstance";
+import { useAuthStore } from "@/store";
 
 /**
  * 토큰 재발급 (POST /api/auth/refresh)
  * @param refreshToken 재발급에 사용할 RefreshToken
  * @returns {Promise<{ accessToken: string; refreshToken: string }>}
  */
-export const refreshToken = async (
-    refreshToken: string
-): Promise<{ accessToken: string; refreshToken: string }> => {
+export const refreshToken = async (refreshToken: string): Promise<{ accessToken: string; refreshToken: string }> => {
     const response = await axiosInstance.post<{ accessToken: string; refreshToken: string }>("/api/auth/refresh", {
         refreshToken,
     });
@@ -28,11 +27,14 @@ export const redirectToKakaoLogin = async (): Promise<string> => {
  * @param code 카카오 인증 후 받은 인가 코드
  * @returns {Promise<{ accessToken: string; refreshToken: string }>}
  */
-export const kakaoLoginCallback = async (
-    code: string
-): Promise<{ accessToken: string; refreshToken: string }> => {
-    const response = await axiosInstance.get<{ accessToken: string; refreshToken: string }>("/api/auth/oauth/kakao/callback", {
-        params: { code },
-    });
+export const kakaoLoginCallback = async (code: string): Promise<{ accessToken: string; refreshToken: string }> => {
+    const response = await axiosInstance.get<{ accessToken: string; refreshToken: string }>(
+        "/api/auth/oauth/kakao/callback",
+        {
+            params: { code },
+        },
+    );
+    const { accessToken, refreshToken } = response.data;
+    useAuthStore.getState().setTokens(accessToken, refreshToken);
     return response.data;
 };
