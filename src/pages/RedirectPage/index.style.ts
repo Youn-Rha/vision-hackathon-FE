52bea429c09a9f219ee15df94e89154a93b4f84f
chominju <cmj021002@knu.ac.kr>
2024-11-16 06:03:21 +0900

    feat: redirectionPage 수정

---
+++ src/pages/RedirectPage/index.style.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+    max-width: 400px;
+    height: min(100vh, 956px);
+    margin: 0 auto;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
